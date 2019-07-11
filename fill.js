b6fe5df20d03db6940255b4316bf07952ff0fd20
Thun <thun@Thuns-MBP.kps.ku.ac.th>
2019-07-11 13:09:14 +0700

    update js

--- fill.js
+++ fill.js
@@ -1,16 +1,90 @@
+
+
+const url =  "https://exceed.superposition.pknn.dev/data/eight"
+
+
 $('#light-switch').ready(function(){
   $('input[type="checkbox"]').click(function(){
       if($(this).prop("checked") == true){
-          alert("Checkbox is checked.");
+        put('LED','on')
       }
       else if($(this).prop("checked") == false){
-          alert("Checkbox is unchecked.");
+        put('LED','off')
+      }
+  });
+});
+
+$('#light-switch').ready(function(){
+  $('input[type="checkbox"]').click(function(){
+      if($(this).prop("checked") == true){
+        put('LED','on')
+      }
+      else if($(this).prop("checked") == false){
+        put('LED','off')
+      }
+  });
+});
+
+$('#light-switch').ready(function(){
+  $('input[type="checkbox"]').click(function(){
+      if($(this).prop("checked") == true){
+        put('LED','on')
+      }
+      else if($(this).prop("checked") == false){
+        put('LED','off')
       }
   });
 });
 
 
 
+
+
+function  get() {
+  fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+  });
+}
+
+// function post(value,status) {
+//   let data =  {"data" : 
+//         {
+          
+//         }}
+//   postData(`${url}`,data)
+//   .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
+//   .catch(error => console.error(error));
+
+//   function postData(url = '', data = {}) {
+//     // Default options are marked with *
+//       return fetch(url, {
+//           method: 'POST', // *GET, POST, PUT, DELETE, et
+//           body: JSON.stringify(data), // body data type must match "Content-Type" header
+//   })
+//       .then(response => response.json()); // parses JSON response into native JavaScript objects 
+//   }
+//   }
+
+
+
+function put(name,status) {
+  fetch((url+ `/${name}`), {
+    method: 'PUT', // *GET, POST, PUT, DELETE, et
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      "value" : `${status}`
+    }), // body data type must match "Content-Type" header
+})
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error('Error:', error))
+}
 
 
 
