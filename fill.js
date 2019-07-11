94302a331a2bd62f86a02b127e559451961419cc
Thun <thun@Thuns-MBP.kps.ku.ac.th>
2019-07-11 11:51:58 +0700

    thun

---
+++ fill.js
@@ -0,0 +1,18 @@
+$('#light-switch').ready(function(){
+  $('input[type="checkbox"]').click(function(){
+      if($(this).prop("checked") == true){
+          alert("Checkbox is checked.");
+      }
+      else if($(this).prop("checked") == false){
+          alert("Checkbox is unchecked.");
+      }
+  });
+});
+
+
+
+
+
+
+
+
